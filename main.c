a16793c091e01951b907e360669a72e6122a59dc
Alyssandro Ramos <dyogo.alyssandro@gmail.com>
2023-05-23 22:25:10 -0300

    Introdução

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <winsock2.h>
+#include "conectorC/include/mysql.h"
+
+int main()
+{
+    
+    mysql_init(NULL);
+    printf("oi");
+
+    return 0;
+}
