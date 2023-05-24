79774f877222871feaacd11af033112f32f4ed72
Alyssandro Ramos <dyogo.alyssandro@gmail.com>
2023-05-24 09:29:54 -0300

    Conexão à DB
    Inicio do desenvolvimento da função para estabelecer a conexão com a DB

--- main.c
+++ main.c
@@ -6,9 +6,31 @@
 
 int main()
 {
+    int conexao();
     
-    mysql_init(NULL);
-    printf("oi");
+    conexao();
 
     return 0;
+}
+
+
+
+int conexao(){
+
+    MYSQL *conn = mysql_init(NULL);
+
+    if(conn == NULL){
+        printf("Erro ao tentar estabelecer a conexão!");
+        exit(1);
+    } 
+
+    if(mysql_real_connect(conn, "localhost", "root", "", "dados_escolares", 0, NULL, 0) == NULL){
+
+        printf("Erro ao estabelecer a conexão com o banco de dados");
+        exit(1);
+
+    }
+
+    printf("tudo okay");
+
 }
