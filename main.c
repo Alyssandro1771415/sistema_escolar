3fc39a88754f9e451b9f27a9b494e514ae786cf8
Alyssandro Ramos <dyogo.alyssandro@gmail.com>
2023-06-07 23:24:28 -0300

    Arrumando os conflitos

--- main.c
+++ main.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include "conectorC/include/mysql.h"
 
-<<<<<<< Updated upstream
-int main()
-{
-    int conexao();
-    
-    conexao();
-=======
 void listClasses(MYSQL *mysql, MYSQL_RES *result, MYSQL_ROW row);
 
 int main() {
@@ -20,34 +12,10 @@ int main() {
     mysql_real_connect(mysql, "localhost", "root", "", "dados_escolares", 0, NULL, 0);
     
     listClasses(mysql, result, row);
->>>>>>> Stashed changes
-
     return 0;
 }
 
 
-<<<<<<< Updated upstream
-
-int conexao(){
-
-    MYSQL *conn = mysql_init(NULL);
-
-    if(conn == NULL){
-        printf("Erro ao tentar estabelecer a conexão!");
-        exit(1);
-    } 
-
-    if(mysql_real_connect(conn, "localhost", "root", "", "dados_escolares", 0, NULL, 0) == NULL){
-
-        printf("Erro ao estabelecer a conexão com o banco de dados");
-        exit(1);
-
-    }
-
-    printf("tudo okay");
-
-}
-=======
 void listClasses(MYSQL *mysql, MYSQL_RES *result, MYSQL_ROW row){
 
     mysql_query(mysql, "select nome_turma from turmas");
