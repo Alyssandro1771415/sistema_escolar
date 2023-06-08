7ffd6d980447789f04da14fa64551cfeb700ec17
Alyssandro Ramos <dyogo.alyssandro@gmail.com>
2023-06-07 23:18:49 -0300

    07/06
    Criação da conexão com a DB e criação do protótipo da função de listagem das turmas.

--- main.c
+++ main.c
@@ -1,19 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
-#include <winsock2.h>
+#include <stdbool.h>
 #include "conectorC/include/mysql.h"
 
+<<<<<<< Updated upstream
 int main()
 {
     int conexao();
     
     conexao();
+=======
+void listClasses(MYSQL *mysql, MYSQL_RES *result, MYSQL_ROW row);
+
+int main() {
+    MYSQL *mysql;
+    MYSQL_RES *result;
+    MYSQL_ROW row;
+    mysql = mysql_init(NULL);
+    mysql_real_connect(mysql, "localhost", "root", "", "dados_escolares", 0, NULL, 0);
+    
+    listClasses(mysql, result, row);
+>>>>>>> Stashed changes
 
     return 0;
 }
 
 
+<<<<<<< Updated upstream
 
 int conexao(){
 
@@ -34,3 +47,21 @@ int conexao(){
     printf("tudo okay");
 
 }
+=======
+void listClasses(MYSQL *mysql, MYSQL_RES *result, MYSQL_ROW row){
+
+    mysql_query(mysql, "select nome_turma from turmas");
+    result = mysql_store_result(mysql);
+
+    int num_fields = mysql_num_fields(result);
+
+    while ((row = mysql_fetch_row(result))) {
+        for (int i = 0; i < num_fields; i++) {
+            printf("%s ", row[i] ? row[i] : "NULL");
+        }
+        printf("\n");
+    }
+    mysql_free_result(result);
+    mysql_close(mysql);
+}
+>>>>>>> Stashed changes
