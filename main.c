8af38c199ecf2226d33489495bf2d57f4e1f842d
Alyssandro Ramos <dyogo.alyssandro@gmail.com>
2023-06-16 09:59:10 -0300

    16/06
    Ajustes iniciais na função de geração do boletim do aluno.

--- main.c
+++ main.c
@@ -3,65 +3,136 @@
 #include <windows.h>
 #include <winsock2.h>
 #include "conectorC/include/mysql.h"
-#include <mysql/mysql.h>
 
-void exportarDadosEstudante();
+MYSQL *mysql;
+MYSQL_RES *result;
+MYSQL_ROW row;
+
+void exportarDadosEstudante(MYSQL *mysql, MYSQL_RES *result, MYSQL_ROW row, char studentName[100]);
 
 int main(){
+
+    mysql = mysql_init(NULL);
+    mysql_real_connect(mysql, "localhost", "root", "", "dados_escolares", 0, NULL, 0);
+
     
     const char *nomeTabela = "nome_da_tabela";
     const char *nomeEstudante = "nome_do_estudante";
 
-    void exportarDadosEstudante();
+    exportarDadosEstudante(mysql, result, row, "Alyssandro 3.1415");
 
     return 0;
 }
 
-void exportarDadosEstudante( const char* nomeTabela, const char* nomeEstudante) {
-    MYSQL *conn;
-    conn = mysql_init(NULL);
+void exportarDadosEstudante(MYSQL *mysql, MYSQL_RES *result, MYSQL_ROW row, char studentName[100]) {
+    char query[200];
+    int classID;
+    int studentID;
+    char className[20];
+    char shift[8];
+    float grades[4];
+    float media[4];
 
-    if (!mysql_real_connect(conn, "localhost", "usuario", "senha", "nomeBanco", 0, NULL, 0)) {
-        fprintf(stderr, "Erro ao conectar ao banco de dados: %s\n", mysql_error(conn));
+    sprintf(query, "SELECT turma_id FROM aluno WHERE nome_aluno = '%s'", studentName);
+    if (mysql_query(mysql, query)) {
+        fprintf(stderr, "Erro ao executar a consulta: %s\n", mysql_error(mysql));
         return;
     }
 
-    char consulta[200];
-    sprintf(consulta, "SELECT * FROM %s WHERE nome = '%s'", nomeTabela, nomeEstudante);
-    if (mysql_query(conn, consulta)) {
-        fprintf(stderr, "Erro ao executar a consulta: %s\n", mysql_error(conn));
+    result = mysql_store_result(mysql); 
+    if (result == NULL) {
+        fprintf(stderr, "Erro ao obter resultados: %s\n", mysql_error(mysql));
         return;
     }
 
-    MYSQL_RES *resultado = mysql_store_result(conn);
-    if (resultado == NULL) {
-        fprintf(stderr, "Erro ao obter resultados: %s\n", mysql_error(conn));
+    row = mysql_fetch_row(result);  
+    if (row == NULL) {
+        fprintf(stderr, "Turma não encontrada.\n");
+        mysql_free_result(result);
+        mysql_close(mysql);
         return;
     }
 
-    MYSQL_ROW registro = mysql_fetch_row(resultado);
-    if (registro == NULL) {
-        fprintf(stderr, "Estudante não encontrado.\n");
-        mysql_free_result(resultado);
-        mysql_close(conn);
+    if (result != NULL)
+    {
+        sscanf(row[0], "%d", &classID);
+        mysql_free_result(result);
+    }
+
+    sprintf(query, "SELECT nome_turma, turno FROM turmas WHERE turma_id = '%i'", classID); 
+
+    if (mysql_query(mysql, query)) {
+        fprintf(stderr, "Erro ao executar a consulta: %s\n", mysql_error(mysql));
         return;
     }
+
+    result = mysql_store_result(mysql);  
+    if (result == NULL) {
+        fprintf(stderr, "Erro ao obter resultados: %s\n", mysql_error(mysql));
+        return;
+    }
+
+    row = mysql_fetch_row(result);  
+    if (row == NULL) {
+        fprintf(stderr, "Dados não obtidos.\n");
+        mysql_free_result(result);
+        mysql_close(mysql);
+        return;
+    }
+
+    if (result != NULL)
+    {
+        strcpy(className, row[0]); 
+        strcpy(shift, row[1]);
+        mysql_free_result(result);
+    } 
+
+    sprintf(query, "SELECT aluno_id FROM aluno WHERE nome_aluno = '%s'", studentName);
+
+        if (mysql_query(mysql, query)) {
+        fprintf(stderr, "Erro ao executar a consulta: %s\n", mysql_error(mysql));
+        return;
+    }
+
+    result = mysql_store_result(mysql);  
+    if (result == NULL) {
+        fprintf(stderr, "Erro ao obter resultados: %s\n", mysql_error(mysql));
+        return;
+    }
+
+    row = mysql_fetch_row(result);  
+    if (row == NULL) {
+        fprintf(stderr, "Dados não obtidos.\n");
+        mysql_free_result(result);
+        mysql_close(mysql);
+        return;
+    }
+
+    if (result != NULL)
+    {
+        sscanf(row[0], "%d", &studentID); 
+        mysql_free_result(result);
+    }
+
+    
+    sprintf(query, "SELECT nota1, nota2, nota3, nota4 FROM notas where aluno_id = '%i'", studentID);
+
+    //--------------------------------------------------------------------
 
     char nomeArquivo[100];
-    sprintf(nomeArquivo, "%s.txt", nomeEstudante);
+    sprintf(nomeArquivo, "%s.txt", studentName);
     FILE *arquivo = fopen(nomeArquivo, "w");
     if (arquivo == NULL) {
         fprintf(stderr, "Erro ao abrir o arquivo %s para escrita.\n", nomeArquivo);
-        mysql_free_result(resultado);
-        mysql_close(conn);
+        mysql_free_result(result);
+        mysql_close(mysql);
         return;
     }
 
-    for (unsigned int i = 0; i < mysql_num_fields(resultado); i++) {
-        fprintf(arquivo, "%s\t", registro[i]);
+    for (unsigned int i = 0; i < mysql_num_fields(result); i++) {
+        fprintf(arquivo, "%s\t", row[i]);
     }
 
     fclose(arquivo);
-    mysql_free_result(resultado);
-    mysql_close(conn);
+    mysql_free_result(result);
 }
