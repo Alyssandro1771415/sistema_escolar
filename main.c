994cde14863845a184f8323f83015b34b555009a
Johnattan-Silva <johnattan.silva.3301@gmail.com>
2023-06-19 16:47:15 -0300

    Mudança geral
    Modificando a estrutura geral para um método mais eficaz com MENU

--- main.c
+++ main.c
@@ -10,67 +10,134 @@ struct aluno{ // POSTERIORMENTE, MODIFICAR PARA QUE OS DADOS DOS ALUNOS SEJAM IM
     int turma_id;
     char turno[10];
     char aluno_nome[100];
+    struct aluno*prox;
+    struct aluno*ant;
 };
 
-struct no{
-    struct aluno info;
-    struct no*prox;
-    struct no*ant;
-};
+struct aluno * inicio;
+struct aluno * fim;
+struct aluno * encontrar(char*);
 
-struct lista{
-    struct no*inicio;
-    struct no*fim;
-};
+void inserir(void), buscar(void), salvar(void);
+void carregar(void), listar(void);
+void deletar(struct aluno **, struct aluno **);
+void armazrnar(struct aluno * i, struct aluno ** inicio, struct aluno ** ultimo);
+void inputs(char *, char *, int), display();
+int menu(void);
 
-void inic_lista(struct lista*lista);
+void main(void){
+    inicio = fim = NULL;
 
-int lista_vazia(struct lista*lista);
+    for(;;){
+        switch(menu()){
+            case 1: inserir();
+                break;
+            case 2: deletar(&inicio, &fim);
+                break;
+            case 3: buscar();
+                break;
+            case 4: listar();
+                break;
+            case 5: salvar();
+                break;    
+            case 6: carregar();
+                break;
+            case 7: exit(0);
+                break;                
+        }
+    }
+}
 
-void inserir_aluno(struct lista*lista, struct aluno aluno);
+// struct no{
+//     struct aluno info;
+//     struct no*prox;
+//     struct no*ant; 
+// };
 
-void limpar_lista(struct lista*lista);
+// struct lista{
+//     struct no*inicio;
+//     struct no*fim;
+// };
+
+// void inic_lista(struct lista*lista);
+
+// int lista_vazia(struct lista*lista);
+
+// void inserir_aluno(struct lista*lista, struct aluno aluno);
+
+// void limpar_lista(struct lista*lista);
+
+void pesq_aluno();
+
+menu(void);
 
 int main(){
-    struct lista lista_dos_alunos;
-    inic_lista(&lista_dos_alunos);
+    // struct lista lista_dos_alunos;
+    // inic_lista(&lista_dos_alunos);
+    menu();
 
     return 0;
 }
 
-void inic_aluno(struct lista*lista){
-    lista->inicio = NULL;
-    lista->fim = NULL;
+// NOVO MENU
+menu(void){
+    char opcao[3];
+    int num;
+
+    printf("1. Inserir aluno\n");
+    printf("2. Deletar alunon\n");
+    printf("3. Buscar por aluno\n");
+    printf("4. Listar arquivo\n");
+    printf("5. Gravar o arquivo\n");
+    printf("6. Carregar o arquivo\n");
+    printf("7. Sair\n");
+    do{
+        printf("\nEscolha: ");
+        gets(opcao);
+        num = atoi(opcao);
+    }while(num<0 || num>7);
+
+    return num;
 }
 
-int lista_vazia(struct lista*lista){
-    return lista->inicio == NULL;
-}
+// void inic_lista(struct lista*lista){
+//     lista->inicio = NULL;
+//     lista->fim = NULL;
+// }
 
-void inserir_aluno(struct lista*lista, struct aluno aluno){
-    struct no*novo_no = (struct no*)malloc(sizeof(struct no));
-    novo_no->info = aluno;
-    novo_no->prox = NULL;
+// int lista_vazia(struct lista*lista){
+//     return lista->inicio == NULL;
+// }
 
-    if(lista_vazia(lista)){
-        novo_no->ant = NULL;
-        lista->inicio = novo_no;
-        lista->fim = novo_no;
-    }else{
-        novo_no->ant = lista->fim;
-        lista->fim->prox = novo_no;
-        lista->fim = novo_no;
-    }
-}
+// void inserir_aluno(struct lista*lista, struct aluno aluno){
+//     struct no*novo_no = (struct no*)malloc(sizeof(struct no));
+//     novo_no->info = aluno;
+//     novo_no->prox = NULL;
 
-void limpar_lista(struct lista*lista){
-    struct no*atual = lista->inicio;
-    while (atual != NULL){
-        struct no*temp = atual;
-        atual = atual->prox;
-        free(temp);
-    }
+//     if(lista_vazia(lista)){
+//         novo_no->ant = NULL;
+//         lista->inicio = novo_no;
+//         lista->fim = novo_no;
+//     }else{
+//         novo_no->ant = lista->fim;
+//         lista->fim->prox = novo_no;
+//         lista->fim = novo_no;
+//     }
+// }
 
-    lista->inicio = NULL;
-    lista->fim = NULL;
+// void limpar_lista(struct lista*lista){
+//     struct no*atual = lista->inicio;
+//     while (atual != NULL){
+//         struct no*temp = atual;
+//         atual = atual->prox;
+//         free(temp);
+//     }
+
+//     lista->inicio = NULL;
+//     lista->fim = NULL;
+// }
+
+void pesq_aluno(void){
+    char nome[40];
+    struct *info, *encontrar();
 }
